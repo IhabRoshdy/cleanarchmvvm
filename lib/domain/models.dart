9af5fc9f778be7e4025cb6cf014cc42ce8a3c8ca
IhabRoshdy <eroushdy@citglobal.com>
2022-06-21 17:44:55 +0200

    created api call

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -13,3 +13,28 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numOfSlides, this.currentIndex);
 }
+
+// Login models
+
+class Customer {
+  String id;
+  String name;
+  int numberOfNotifications;
+
+  Customer(this.id, this.name, this.numberOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
